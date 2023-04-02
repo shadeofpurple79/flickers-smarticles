0847235cef381ec9e2a49d463177fe6013a30b9a
shadeofpurple79 <aomoss@yahoo.com>
2023-04-02 16:46:08 +0000

    Get api url from opentdb trivia db

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import json
+import requests
+
+# category: entertainmewnt - film, 20 questions, multiple choice, from opentdb api
+api_url = "https://opentdb.com/api.php?amount=20&category=11&type=multiple"
