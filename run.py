b82d8bbc45a6917626bdee90fa3a7e6b608c6b7f
shadeofpurple79 <aomoss@yahoo.com>
2023-04-03 11:09:33 +0000

    Get quizk questions and answers from open t db

--- run.py
+++ run.py
@@ -3,3 +3,10 @@ import requests
 
 # category: entertainmewnt - film, 20 questions, multiple choice, from opentdb api
 api_url = "https://opentdb.com/api.php?amount=20&category=11&type=multiple"
+
+# get data from the API
+response = requests.get(api_url)
+data = json.loads(response.text)
+
+# create variable to store quiz questions
+questions = data["results"]
