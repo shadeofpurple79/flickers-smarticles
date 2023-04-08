2600b0af52405d8ea7b35ca107b2bc882b36de9c
shadeofpurple79 <aomoss@yahoo.com>
2023-04-08 22:01:35 +0000

    Write game flow and functions to be created

--- run.py
+++ run.py
@@ -12,3 +12,26 @@ data = json.loads(response.text)
 questions = data["results"]
 
 print(data)
+
+# show game intro at the start
+
+# get 20 random questions 
+# get the answer options
+# get the correct answer for each question
+
+# question number is zero
+# score is zero
+
+# function to ask question 
+    # from 1 to 20
+    # select 1 random question
+    # show the selected question, 4 answer options
+    # increase question number by 1 out of 20
+    # ask player to input their answer
+        # function to validate answer
+            # while input is not A or B or C or D; and a or b or c or d
+            # print message invalid answer, enter A B C D, ask for input
+            # when input is A or B or C or D; and a or b or c or d; go to unfciont to check answer
+
+# function to check answer
+    # 
