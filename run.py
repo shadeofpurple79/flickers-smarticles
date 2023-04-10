f6161384ea03a2917e183564292a41f7412c6171
shadeofpurple79 <aomoss@yahoo.com>
2023-04-10 16:07:47 +0000

    Show a random question and  answers, add colours

--- run.py
+++ run.py
@@ -1,33 +1,55 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
 import json
 import requests
 
-# category: entertainmewnt - film, 20 questions, multiple choice, from opentdb api
-api_url = "https://opentdb.com/api.php?amount=20&category=11&type=multiple"
+# from opentdb api, category: entertainment - film, 20 questions, multiple choice
+API_URL = "https://opentdb.com/api.php?amount=20&category=11&type=multiple"
 
 # get data from the API
-response = requests.get(api_url)
+response = requests.get(API_URL)
 data = json.loads(response.text)
 
 # create variable to store quiz questions
 questions = data["results"]
 
-print(data)
+# print(data)
+
+correct_answers = 0 #score is zero at the start
+
+# function to ask question 
+for question in questions: #for each question in the random 20 questions in the api
+    print(Style.BRIGHT + Back.MAGENTA + question["question"]) #print a random question to the player
+    print("Select your answer from the following 4 options:") #print the answers for the question to the player
+    for i in range(len(question["incorrect_answers"])): #len() function returns the number of items in an object, 20 questions, 
+        print(f"{i+1}. {question['incorrect_answers'][i]}")
+    print(f"{len(question['incorrect_answers'])+1}. {question['correct_answer']}")
+    user_answer = int(input("What\'s your answer? Select 1,2,3 or 4: ")) #player selects an answer: 1,2,3, or 4 for each question
+    if user_answer == len(question["incorrect_answers"]) + 1:
+        print(Fore.GREEN + "Correct, well done!\n\n")
+        correct_answers += 1 #increase score by 1 for each correct answer
+    else:
+        print(Fore.RED + "Sorry, incorrect.\n\n")
+
+print(f"You got {correct_answers} out of {len(questions)} questions correct.")
 
 # show game intro at the start
 
-# get 20 random questions 
-# get the answer options
-# get the correct answer for each question
+ 
+
+
 
 # question number is zero
-# score is zero
 
-# function to ask question 
+
+
     # from 1 to 20
     # select 1 random question
     # show the selected question, 4 answer options
     # increase question number by 1 out of 20
-    # ask player to input their answer
+    
         # function to validate answer
             # while input is not A or B or C or D; and a or b or c or d
             # print message invalid answer, enter A B C D, ask for input
