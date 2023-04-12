f1fb07a8911b1a0f420da2e3199916b5677d8e10
shadeofpurple79 <aomoss@yahoo.com>
2023-04-12 10:34:44 +0000

    Add function to start game and to validate answer

--- run.py
+++ run.py
@@ -5,17 +5,6 @@ colorama.init(autoreset=True) #return to default colour after each time
 import json
 import requests #Make a request to a web page, and print the response text
 
-# function to validate user answer
-def validate_choice(user_answer, choices):
-    try:
-        if user_input not in choices:
-            raise ValueError
-    except ValueError:
-        clear()
-        print(red(LINE))
-        print(red(CENT(f'Error: "{user_answer}" is not valid! Enter 1 or 2 or 3 or 4')))
-        return False
-    return True
 
 def start_game():
     # from opentdb api, category: entertainment - film, 20 questions, multiple choice
@@ -33,30 +22,55 @@ def start_game():
     correct_answers = 0 #score is zero at the start, score equals number of correct answers 
     choices = ["1", "2", "3", "4"]
     for question in questions: #for each question in the random 20 questions in the api
-        print(Style.BRIGHT + Back.MAGENTA + question["question"].replace("&quot;", "'").replace("&#039;", "'").replace("&ldquo;", "'")) #print a random question to the player, remove ugly text that comes in from the api for apostrophes
+        print(Style.BRIGHT + Back.MAGENTA + question["question"].replace("&quot;", "'").replace("&#039;", "'").replace("&ldquo;", "'").replace(",&rdquo;", "'")) #print a random question to the player, remove ugly text that comes in from the api for apostrophes
         print("Select your answer from the following 4 options:") #print the answers for the question to the player
         for i in range(len(question["incorrect_answers"])): #len() function returns the number of items in an object, 20 questions, 
             print(f"{i+1}. {question['incorrect_answers'][i]}")
         print(f"{len(question['incorrect_answers'])+1}. {question['correct_answer']}")
-        user_answer = int(input("What\'s your answer? Select 1,2,3 or 4: ")) #player selects an answer: 1,2,3, or 4 for each question
+        user_answer = (input("What\'s your answer? Select 1,2,3 or 4: ")) #player selects an answer: 1,2,3, or 4 for each question
         # while not (user_answer == 1 or user_answer == 2 or user_answer == 3 or user_answer == 4):
-            # user_answer = int(input("You can only enter 1, 2, 3 or 4. Try again: "))
-        validate_choice(user_answer, choices)
+            
+        def validate_choice(user_answer, choices):
+            try:
+                if user_answer not in choices:
+                    user_answer = (input("You can only enter 1, 2, 3 or 4. Try again: "))
+                    # raise ValueError
+            except ValueError:
+                print(red(LINE))
+                print(red(CENT(f'Error: "{user_answer}" is not valid! Enter 1 or 2 or 3 or 4')))
+                return False
+            return True
+
+        
         if user_answer == len(question["incorrect_answers"]) + 1:
             print(Fore.GREEN + "Correct, well done!\n\n")
+            print()  # Add a blank line between questions
             correct_answers += 1 #increase score by 1 for each correct answer
         else:
             print(Fore.RED + "Sorry, incorrect.\n\n")
+            print()  # Add a blank line between questions
     print(Back.MAGENTA + "GAME OVER!")
     print(f"You got {correct_answers} out of {len(questions)} questions correct.")
 
 start_game()
 
 
+# function to validate user answer
+# def validate_choice(user_answer, choices):
+#     try:
+#         if user_answer not in choices:
+#             raise ValueError
+#     except ValueError:
+#         print(red(LINE))
+#         print(red(CENT(f'Error: "{user_answer}" is not valid! Enter 1 or 2 or 3 or 4')))
+#         return False
+#     return True
+
 
     #EXCEPTION 1 TO HANDLE: player enters numbers outside of 1234
     #EXCEPTION 2 TO HANDLE: player enters non-integer
     # bug remove unwanted code from all answer choices and replace with apostrophe
+    # bug validation not working, not throwing an error when answer is other than 1234
 
     # while True: 
     #     user_answer = ""
@@ -73,24 +87,4 @@ start_game()
     #         user_answer = int(input("You can only enter 1, 2, 3 or 4. Try again: "))
 
 
-
-
-
 # show game intro at the start
-
- 
-
-
-
-# question number is zero
-
-
-
-    # from 1 to 20
-    # select 1 random question
-    # show the selected question, 4 answer options
-    # increase question number by 1 out of 20
-    
- 
-# function to check answer
-    # 
